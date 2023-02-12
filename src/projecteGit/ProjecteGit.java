da6e65a921954d3880139267ccb53e8fa02999e7
Marti Dangla <danglamarti98@gmail.com>
2023-02-12 18:04:54 +0100

    Primeres classes

--- src/projecteGit/ProjecteGit.java
+++ src/projecteGit/ProjecteGit.java
@@ -1,9 +1,38 @@
 package projecteGit;
 
+import java.io.IOException;
+
 public class ProjecteGit {
 
+	boolean esPrimo(int num) {
+		if (num <= 1) return false;
+		int i = 2;
+		while (i <= Math.sqrt(num)) {
+			if (num % i == 0) return false;
+			i++;
+		}
+		return true;
+	}
+	
+	static int leerEntero(String texto, int min, int max) throws NumberFormatException, IOException {
+		
+		return 1;
+		
+	}
+	
 	public static void main(String[] args) {
 
+		ProjecteGit programa = new ProjecteGit();
+
+		// Leer el número
+		int num = programa.leerEntero("Entra un número : ",1,Integer.MAX_VALUE);
+
+		if (programa.esPrimo(num)) {
+			System.out.println("Si es primo");
+		} else {
+			System.out.println("No es primo");
+		}
+		
 	}
 
 }
